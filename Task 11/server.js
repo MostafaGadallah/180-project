e0367aa0e01ab213f8afac4c10e09e8276055593
MostafaGadallah <m.mahmoud10907@gmail.com>
2026-07-01 05:17:46 +0200

    add back end basic part

---
+++ Task 11/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import mysql from "mysql2";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "testdb",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Connected to MySQL");
+});
+
+app.post("/next", (req, res) => {
+  const { name, email, number, university, level, faculty  } = req.body;
+
+  const sql = "INSERT INTO users (name, email, phone_number, university, academic_level, faculty) VALUES (?, ?, ?, ?, ?, ?)";
+
+  db.query(sql, [name, email, number, university, level, faculty], (err, result) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
+
+    res.json({
+      message: "User added successfully",
+    });
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
--- Task 11/src/Form.jsx
+++ Task 11/src/Form.jsx
@@ -1,5 +1,34 @@
-function Form() {
+import { useState } from "react";
 
+function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [university, setUniversity] = useState("");
+  const [level, setLevel] = useState("");
+  const [faculty, setFaculty] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    alert("Submitting...");
+    const response = await fetch("http://localhost:3000/next", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        number,
+        university,
+        level,
+        faculty,
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
     return (
         <>
             <div className="">
@@ -7,30 +36,30 @@ function Form() {
                     <h3 className="text-lg font-bold">Cautions</h3>
                     <p className="text-gray-500 my-2">Be sure to check your E-mail after submitting the form</p>
                 </div>
-                <form className="form flex flex-col gap-3 max-w-[600px] mx-auto" action="">
+                <form onSubmit={handleSubmit} className="form flex flex-col gap-3 max-w-[600px] mx-auto" action="">
                     <label htmlFor="name" className="text-left">Name</label>
-                    <input type="text" placeholder="name" className="border border-gray-600 outline-none  p-3" />
+                    <input value={name} onChange={(e) => setName(e.target.value)} name="name" type="text" placeholder="name" className="border border-gray-600 outline-none  p-3" />
                     <label htmlFor="email" className="text-left">E-mail</label>
-                    <input type="email" placeholder="E-mail" className="border border-gray-600 outline-none  p-3" />
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} name="email"  type="email" placeholder="E-mail" className="border border-gray-600 outline-none  p-3" />
                     <label htmlFor="number" className="text-left">Phone Number</label>
-                    <input type="text" placeholder="Phone Number" className="border border-gray-600 outline-none  p-3" />
+                    <input value={number} onChange={(e) => setNumber(e.target.value)} name="number" type="text" placeholder="Phone Number" className="border border-gray-600 outline-none  p-3" />
                     <label htmlFor="universty" className="text-left">Universty</label>
-                    <select name="universty" id="" className="text-left border border-gray-600 outline-none  p-3">
+                    <select value={university} onChange={(e) => setUniversity(e.target.value)} name="universty" id="" className="text-left border border-gray-600 outline-none  p-3">
                         <option value="Helwan">Select an option:</option>
                         <option value="Helwan">Helwan</option>
                         <option value="other">Other</option>
                     </select>
                     <label htmlFor="level" className="text-left">Level</label>
-                    <select name="level" id="" className="text-left border border-gray-600 outline-none  p-3">
+                    <select value={level} onChange={(e) => setLevel(e.target.value)} name="level" id="" className="text-left border border-gray-600 outline-none  p-3">
                         <option value="Helwan">Select an option:</option>
-                        <option value="Helwan">1</option>
-                        <option value="other">2</option>
-                        <option value="other">3</option>
-                        <option value="other">4</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
                     </select>
                     <label htmlFor="faculty" className="text-left">College / Faculty</label>
-                    <input type="text" placeholder="College / Faculty" className="border border-gray-600 outline-none  p-3" />
-                    <button type="submit" className="bg-[#462c38] text-white py-3 " >Submit</button>
+                    <input value={faculty} onChange={(e) => setFaculty(e.target.value)}name="faculty" type="text" placeholder="College / Faculty" className="border border-gray-600 outline-none  p-3" />
+                    <button type="submit" className="bg-[#462c38] text-white py-3 ">Submit</button>
                 </form>
             </div>
         </>
